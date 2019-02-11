468c2a03065340fda8347b4b447fd93e5d1737b5
Maggi Sledjeski <sledjeski_ma@gsc_405_05.mercer.edu>
2019-02-11 16:50:27 -0500

    do notes on main.cpp to add dfr math

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #ifndef MAIN
 #define MAIN
 
+/*DFR = 100ish do the math with the final caluclations then solve for dfr to plug into lockrate
+ * dfr = FPP/SPP */
+
 #include "includes.h"
-//#include "structs.h"
 #include "prototypes.h"
 #include "globals.h"
 
@@ -87,8 +89,8 @@ void PeriodTime()
     extern double omega;
     extern bool sc2;
     extern char osign2;
-    extern int opt;
-    extern int tpt;
+    //extern int opt;
+    //extern int tpt;
     extern int pframes;
     extern double t;
     extern double oldppt;
@@ -100,70 +102,70 @@ void PeriodTime()
     double currpt = t;
 	
 	/*current period time*/
-    int ct = glutGet(GLUT_ELAPSED_TIME);
+    //int ct = glutGet(GLUT_ELAPSED_TIME);
 
     if(omega <= -0.0 && osign2 == 'n')
     {
         osign2 = 'n';
-        tpt = tpt + (ct - opt);
+        //tpt = tpt + (ct - opt);
         pttotal = pttotal + (currpt - oldppt);
-        opt = ct;
+        //opt = ct;
         oldppt = currpt;
     } else if(omega > 0.0 && osign2 == 'p')
     {
         osign2 = 'p';
-        tpt = tpt + (ct - opt);
-        opt = ct;
+        //tpt = tpt + (ct - opt);
+        //opt = ct;
         pttotal = pttotal + (currpt - oldppt);
         oldppt = currpt;
     } else if(omega <= -0.0 && osign2 == 'p' && sc3 == false && sc4 == false)
     {
         osign2 = 'n';
         sc3 = true;
-        tpt = tpt + (ct - opt);
-        opt = ct;
+        //tpt = tpt + (ct - opt);
+        //opt = ct;
         pttotal = pttotal + (currpt - oldppt);
         oldppt = currpt;
     } else if(omega > 0.0 && osign2 == 'n' && sc3 == false && sc4 == false)
     {
         osign2 = 'p';
         sc3 = true;
-        tpt = tpt + (ct - opt);
-        opt = ct;
+        //tpt = tpt + (ct - opt);
+        //opt = ct;
         pttotal = pttotal + (currpt - oldppt);
         oldppt = currpt;
     } else if(omega <= -0.0 && osign2 == 'p' && sc3 == true && sc4 == false)
     {
 		osign2 = 'n';
-        tpt = tpt + (ct - opt);
+        //tpt = tpt + (ct - opt);
         pttotal = pttotal + (currpt - oldppt);
-        cout << "spp(tpt): " << tpt << endl;
+        //cout << "spp(tpt): " << tpt << endl;
         cout << "pframes: " << pframes << endl;
         cout << "actual pend period: " << pttotal << endl;
         totalFPP = pframes;
         totalPTime = pttotal;
         sc3 = false;
-        tpt = 0;
+        //tpt = 0;
         pttotal = 0;
         oldppt = currpt;
-        opt = ct;
+        //opt = ct;
         pframes = 0;
 		//cout << " " << endl;
     } else if(omega > 0.0 && osign2 == 'n' && sc3 == true && sc4 == false)
     {
         osign2 = 'p';
-        tpt = tpt + (ct - opt);
+        //tpt = tpt + (ct - opt);
         pttotal = pttotal + (currpt - oldppt);
-        cout << "spp(tpt): " << tpt << endl;
+        //cout << "spp(tpt): " << tpt << endl;
         cout << "pframes: " << pframes << endl;
         cout << "actual pend period: " << pttotal << endl;
         totalFPP = pframes;
         totalPTime = pttotal;
         sc3 = false;
-        tpt = 0;
+        //tpt = 0;
         pttotal = 0;
         oldppt = currpt;
-        opt = ct;
+        //opt = ct;
         pframes = 0;
 		//cout << " " << endl;
     }
